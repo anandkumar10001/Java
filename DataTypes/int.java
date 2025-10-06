9fdf975c2f1d9c6ef6478ec55440a0b7d375156e
anandkumar10001 <anand10001kumar@gmail.com>
2025-10-06 21:30:06 +0530

    Update int.java

---
+++ DataTypes/int.java
@@ -0,0 +1,13 @@
+//Byte
+public class ByteExample {
+    public static void main(String[] args) {
+        byte a = 10;
+        byte b = -20;
+        byte c = 127;
+        // byte d = 128;  // Error: out of range
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+    }
+}
