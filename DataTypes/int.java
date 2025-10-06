dc5e40f5c1e2e5e253d6bff552f5e0f4274a7d61
anandkumar10001 <anand10001kumar@gmail.com>
2025-10-06 21:37:36 +0530

    Update int.java

--- DataTypes/int.java
+++ DataTypes/int.java
@@ -1,13 +1,22 @@
-//Byte
+
 public class ByteExample {
     public static void main(String[] args) {
+//Byte
         byte a = -128; //min
         byte b = 20;
         byte c = 127; //max
         // byte d = 128;  // Error: out of range
-
         System.out.println("a = " + a);
         System.out.println("b = " + b);
         System.out.println("c = " + c);
+
+//Short
+        short num1 = 1000;
+        short num2 = -2000;
+        System.out.println("num1: " + num1);
+        System.out.println("num2: " + num2);
+    
     }
 }
+
+        
